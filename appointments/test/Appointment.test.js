82001914ee19144b62d6d9e30243b3e7418624fc
Jordi Anguela <jordi@codium.team>
2019-12-23 17:04:46 +0100

    renders another customer first name

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -12,14 +12,12 @@ describe('Appointment', () => {
         expect(container.textContent).toMatch('Ashley');
     });
 
-    it.skip('renders another customer first name', () => {
+    it('renders another customer first name', () => {
         const customer = {firstName: 'Jordan'};
-        const component = <Appointment customer={customer}/>;
         const container = document.createElement('div');
-        document.body.appendChild(container);
 
-        ReactDOM.render(component, container);
+        ReactDOM.render(<Appointment customer={customer}/>, container);
 
-        expect(document.body.textContent).toMatch('Jordan');
+        expect(container.textContent).toMatch('Jordan');
     });
 });
