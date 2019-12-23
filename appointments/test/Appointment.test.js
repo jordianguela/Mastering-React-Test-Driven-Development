32905b7ecba0e369c06d5d78bcf7f31f717e7f6c
Jordi Anguela <jordi@codium.team>
2019-12-23 17:16:23 +0100

    refactor: reduce code duplication

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -3,20 +3,27 @@ import ReactDOM from 'react-dom';
 import {Appointment} from '../src/Appointment';
 
 describe('Appointment', () => {
-    it('renders the customer first name', () => {
-        const customer = {firstName: 'Ashley'};
-        const container = document.createElement('div');
+    let customer;
+    let container;
 
-        ReactDOM.render(<Appointment customer={customer}/>, container);
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    const render = component => ReactDOM.render(component, container);
+
+    it('renders the customer first name', () => {
+        customer = {firstName: 'Ashley'};
+
+        render(<Appointment customer={customer}/>);
 
         expect(container.textContent).toMatch('Ashley');
     });
 
     it('renders another customer first name', () => {
-        const customer = {firstName: 'Jordan'};
-        const container = document.createElement('div');
+        customer = {firstName: 'Jordan'};
 
-        ReactDOM.render(<Appointment customer={customer}/>, container);
+        render(<Appointment customer={customer}/>);
 
         expect(container.textContent).toMatch('Jordan');
     });
